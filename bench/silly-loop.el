29c1a6d25b382c3cd96ecd96fbb9e22b568e4f31
Dmytro S <me@dmytrish.net>
2018-07-08 14:43:06 +0200

    npm run bench

--- bench/silly-loop.el
+++ bench/silly-loop.el
@@ -1,1 +1,1 @@
-((lambda (n) (let ((t1 (float-time))) (while (<= 0 (setq n (- n 1)))) (- (float-time) t1))) 100000000)
+((lambda (n) (let ((t1 (float-time))) (while (<= 0 (setq n (- n 1)))) (print (- (float-time) t1)))) 100000000)
