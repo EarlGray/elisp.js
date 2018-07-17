4841352c9259b56e982ee4aa423c9cd1b7a45ddd
Dmytro S <me@dmytrish.net>
2018-07-17 21:45:45 +0200

    moved demo/; repo url in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/demo.js',
+  entry: './demo/demo.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
